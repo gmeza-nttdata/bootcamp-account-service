d60bfd64c93195d6a93b7822ac22b5315ce562d5
gmeza-nttdata <94468068+gmeza-nttdata@users.noreply.github.com>
2021-11-30 01:59:32 -0500

    Account Contract

--- src/main/java/com/nttdata/bootcamp/accountservice/domain/Account.java
+++ src/main/java/com/nttdata/bootcamp/accountservice/domain/Account.java
@@ -9,14 +9,23 @@ import java.util.List;
 @Data
 public class Account {
 
+	/** account number or id. */
 	private String number;
+	/** main holder. */
 	private Integer userId;
+	/** account type. */
 	private AccountType type;
+	/** Currency of the account. */
 	private String currencyName;
+	/** Current Account balance. */
 	private BigDecimal balance;
+	/** Account Contract. */
+	private AccountContract contract;
 
 	// Only for Business:
+	/** Holders list. */
     private List<Integer> holders;
+    /** Singers list. */
     private List<Integer> signers;
 
 }
